e8270459d59e30904e161d28a4b0a3d1e55fc21b
Cameron Abel <cameronabel@gmail.com>
2022-12-18 18:00:20 -0800

    Failed new mobs inventory test

--- __tests__/classTemplate.test.js
+++ __tests__/classTemplate.test.js
@@ -1,7 +1,7 @@
 import { Entity, Mobile } from "../src/classTemplate";
 
 describe('Entity', () => {
-  test ('It should return an Entity object with a name and health value.', () => {
+  test('It should return an Entity object with a name and health value.', () => {
     const entity = new Entity('test object', 100);
     expect(entity.name).toEqual('test object');
     expect(entity.health).toEqual(100);
@@ -9,9 +9,13 @@ describe('Entity', () => {
 })
 
 describe('Mobile', () => {
-  test ('It should return a Mobile object with a name and health value.', () => {
-    const mobile = new Mobile('test object', 100);
-    expect(mobile.name).toEqual('test object');
+  test('It should return a Mobile object with a name and health value.', () => {
+    const mobile = new Mobile('test mob', 100);
+    expect(mobile.name).toEqual('test mob');
     expect(mobile.health).toEqual(100);
+  });
+  test('A new mobile should have an empty inventory by default.', () => {
+    const mobile = new Mobile('test mob', 100);
+    expect(mobile.inventory).toHaveLength(0);
   });
 })
