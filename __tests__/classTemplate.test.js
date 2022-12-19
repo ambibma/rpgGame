f51d106a3c1f833a657f7eb21b05e13c798ef4d6
ambi hidalgo <ambio.pk@gmail.com>
2022-12-18 16:38:45 -0800

    Add classTemplate.js, enemy.js, item.js, player.js, add tests
    Co-authored-by: Cameron Abel <cameronabel@gmail.com>
    Co-authored-by: Emil Anton <emilbstn@gmail.comemilbstn@gmail.com>

---
+++ __tests__/classTemplate.test.js
@@ -0,0 +1,5 @@
+import Entity from "../src/classTemplate";
+
+describe('Entity', () => {
+  
+})
---
+++ src/classTemplate.js
@@ -0,0 +1,15 @@
+export default class Entity {
+  constructor(name, health){
+    // this.inventory = [];
+    // this.action
+    this.name = name;
+    this.health = health;
+  }
+}
+//health: 100
+//inventory: ['stick']
+//action: [movement, attack, takeDamage, death, flea,]
+//name: 'Jared'
+
+
+//testEnemy = new Entity('bob', 100)
