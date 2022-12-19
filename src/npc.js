97044a49515a2fbae1ad9fb94a866260147d22e8
Cameron Abel <cameronabel@gmail.com>
2022-12-18 20:06:54 -0800

    Passing NPC creation test

--- src/npc.js
+++ src/npc.js
@@ -2,6 +2,6 @@ import { Mobile } from "./classTemplate";
 
 export default class NPC extends Mobile {
   constructor(level=1, name='test npc', health=100) {
-    //super(level, name, health);
+    super(level, name, health);
   }
 }
