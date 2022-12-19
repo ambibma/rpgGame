370622a131669a5d95caf4b61ad7a0d7b716b919
Cameron Abel <cameronabel@gmail.com>
2022-12-18 17:49:02 -0800

    Failing Mobile creation test

--- src/classTemplate.js
+++ src/classTemplate.js
@@ -1,9 +1,16 @@
-export default class Entity {
+export { Entity, Mobile };
+
+class Entity {
   constructor(name, health){
-    // this.inventory = [];
-    // this.action
     this.name = name;
     this.health = health;
+  }
+}
+
+class Mobile extends Entity {
+  constructor(name, health) {
+    //super(name, health);
+    //self.inventory = []
   }
 }
 //health: 100
